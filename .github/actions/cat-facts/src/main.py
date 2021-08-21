c641bd6fa222c8674f636be397a5205ba49ee649
Dmitry Volodin <mr.molkree@gmail.com>
2021-08-21 20:44:40 +0300

    Sleep and print about it

--- .github/actions/cat-facts/src/main.py
+++ .github/actions/cat-facts/src/main.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 # Make an HTTP GET request to the cat-fact API
 cat_url = "https://catfact.ninja/fact"
@@ -7,6 +8,11 @@ random_fact = r.json()["fact"]
 
 # Print the individual randomly returned cat-fact
 print(random_fact)
+
+sleep_time = 5
+time.sleep(sleep_time)
+
+print("I've just slept for {sleep_time}")
 
 # Set the fact-output of the action as the value of random_fact
 print(f"::set-output name=fact::{random_fact}")
